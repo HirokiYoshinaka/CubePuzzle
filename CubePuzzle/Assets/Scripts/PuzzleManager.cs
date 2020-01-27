7086075d33c360690f1683380c22dcf2294e7b2e
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2020-01-27 18:12:15 +0900

    Add a constant value and two enumerations
    Length of one side , six sides and colors

--- CubePuzzle/Assets/Scripts/PuzzleManager.cs
+++ CubePuzzle/Assets/Scripts/PuzzleManager.cs
@@ -7,15 +7,27 @@ using UnityEngine;
 /// </summary>
 public class PuzzleManager : MonoBehaviour
 {
+    static readonly int cubeLength = 3;
+    // 6面
+    enum SixSides
+    {
+        Top = 0, Under, Right, Left, Front, Back, MAX
+    }
+    // 6色
+    enum SixColors
+    {
+        White, Orange, Red, Green, Yellow, Blue, MAX
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
