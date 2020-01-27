ab2642c3ad89b5489c1004717f77ee69bdcba627
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2020-01-27 18:01:15 +0900

    Add new script

---
+++ CubePuzzle/Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// パズルのデータ、ロジックの管理クラス
+/// </summary>
+public class PuzzleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
