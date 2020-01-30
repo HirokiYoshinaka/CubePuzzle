6d21958aaa28512379417248047634288f4b6aa0
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2020-01-31 05:13:24 +0900

    Add InputManager class

---
+++ CubePuzzle/Assets/Scripts/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ユーザーの入力に応じた処理を行います
+/// </summary>
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
