eebd4a96d03aed450845d3dc8841bdd91630d207
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2020-01-31 05:24:09 +0900

    Add ArondCamara class

---
+++ CubePuzzle/Assets/Scripts/ArondCamara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  カメラの回転移動を制御します。
+/// </summary>
+public class ArondCamara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
